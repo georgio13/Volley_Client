2c678a353b6d53d98fd39a5687924e3d8aecdb6a
Yorgos Chatziparaskevas <georgio13@users.noreply.github.com>
2024-02-01 03:56:57 +0200

    Update assets

--- src/app/game-page/game-page.component.ts
+++ src/app/game-page/game-page.component.ts
@@ -5,6 +5,74 @@ import {Component} from '@angular/core';
   templateUrl: './game-page.component.html'
 })
 export class GamePageComponent {
+  public teams: any[];
+
   constructor() {
+    this.teams = [
+      {
+        name: 'Αργεντινή',
+        value: 'arg'
+      },
+      {
+        name: 'Βραζιλία',
+        value: 'bra'
+      },
+      {
+        name: 'Βουλγαρία',
+        value: 'bul'
+      },
+      {
+        name: 'Καναδάς',
+        value: 'can'
+      },
+      {
+        name: 'Κούβα',
+        value: 'cub'
+      },
+      {
+        name: 'Γαλλία',
+        value: 'fra'
+      },
+      {
+        name: 'Γερμανία',
+        value: 'ger'
+      },
+      {
+        name: 'Ιράν',
+        value: 'iri'
+      },
+      {
+        name: 'Ιταλία',
+        value: 'ita'
+      },
+      {
+        name: 'Ιαπωνία',
+        value: 'jpn'
+      },
+      {
+        name: 'Ολλανδία',
+        value: 'ned'
+      },
+      {
+        name: 'Πολωνία',
+        value: 'pol'
+      },
+      {
+        name: 'Σερβία',
+        value: 'srb'
+      },
+      {
+        name: 'Σλοβενία',
+        value: 'slo'
+      },
+      {
+        name: 'Τουρκία',
+        value: 'tur'
+      },
+      {
+        name: 'ΗΠΑ',
+        value: 'usa'
+      }
+    ];
   }
 }
